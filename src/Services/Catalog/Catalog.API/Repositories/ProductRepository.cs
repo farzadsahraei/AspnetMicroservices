7d2d94ebff24d21513870a360c83a86d0ae65a47
Farzad Sahraei <f.sahraei@gmail.com>
2023-04-11 19:16:10 +0330

    CatalogApi

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository:IProductRepository
+    {
+        ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+             return await  _context.Products.Find(p => true).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string category)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p=> p.Category,category);
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<Product> GetProductById(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+            return await _context.Products.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public Task CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
